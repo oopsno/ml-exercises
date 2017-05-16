c02e1f92703415f90546953820e942d0b43cc803
YufeiLiu <yufeiliu94@gmail.com>
2017-05-16 22:00:37 +0800

    let's make a new start

--- include/mlexerices/algorithm.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef ML_EXERCISES_ALGORITHM_HPP
-#define ML_EXERCISES_ALGORITHM_HPP
-
-#include <armadillo>
-
-arma::vec linear_regression(const arma::mat &x, const arma::mat &y, const double alpha);
-
-#endif //ML_EXERCISES_ALGORITHM_HPP
